1c90180ed8da1e40d0c25345579c5bacf6fe1040
Mide <akinwumiolumide5@gmail.com>
2025-09-15 17:16:14 +0100

    feat: implement smooth scrolling on page load

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,12 +1,19 @@
+'use client'
+
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import '../app/globals.css'
+import { useEffect } from 'react'
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }, [])
+
   return (
     <html lang='en'>
       <body>
